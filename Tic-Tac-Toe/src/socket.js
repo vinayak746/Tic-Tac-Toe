ce0720fc3a6915c47c2fe7a5514702ba10548a4c
Vinayak Arora <161114357+vinayak746@users.noreply.github.com>
2025-11-06 13:58:14 +0530

    Added the backend url in socket

--- Tic-Tac-Toe/src/socket.js
+++ Tic-Tac-Toe/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://tic-tac-toe-3iod.onrender.com";
 const socket = io(backendUrl, {
   autoConnect: false,
 });
